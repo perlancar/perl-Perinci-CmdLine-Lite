5d9351f071622bdb6e8cd856265a0c33bc2d3963
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-04-15 14:32:45 +0700

    Add basic support for --format=vd

--- lib/Perinci/CmdLine/Lite.pm
+++ lib/Perinci/CmdLine/Lite.pm
@@ -29,7 +29,7 @@ has validate_args => (
     default => 1,
 );
 
-my $formats = [qw/text text-simple text-pretty json json-pretty csv termtable html html+datatables perl/];
+my $formats = [qw/text text-simple text-pretty json json-pretty csv termtable html html+datatables perl vd/];
 
 sub BUILD {
     my ($self, $args) = @_;
@@ -371,6 +371,13 @@ sub hook_format_result {
         $fmt = 'text-pretty';
         no warnings 'once';
         $ENV{FORMAT_PRETTY_TABLE_BACKEND} //= 'Term::TablePrint';
+    } elsif ($fmt eq 'vd') { # view in Visidata
+        $fmt = 'text-pretty';
+        $ENV{VIEW_RESULT} //= 1;
+        no warnings 'once';
+        $Perinci::CmdLine::Base::tempfile_opt_suffix = '.csv';
+        $ENV{FORMAT_PRETTY_TABLE_BACKEND} //= 'Text::Table::CSV';
+        $r->{viewer} //= 'vd';
     }
 
     my $fres = Perinci::Result::Format::Lite::format(
